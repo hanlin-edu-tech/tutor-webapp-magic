0589b2a06a50da9edb99b3c9cd230cc7832d8751
infinityAlive <sonic@ehanlin.com.tw>
2018-09-12 12:47:20 +0800

    調整 jquery target 的取得方式，些許優化

--- src/js/galaxy-space/event-chest-get.js
+++ src/js/galaxy-space/event-chest-get.js
@@ -10,18 +10,31 @@ define(['jquery', 'ajax', 'eventChestBtnOn'], ($, ajax, eventChestBtnOn) => {// 
       for (let index in chests) {
         let chest = chests[index]
         let targets = {}
+        let currentChest
 
         targets.platform = $(`#section_middle_part .potion.platform-${chest.colorPlatform}`)
-
         targets.platform
           .append(`<img src="https://s3-ap-northeast-1.amazonaws.com/ehanlin-web-resource/event-space/img/magicImg/LV${chest.level}.png">`)
 
-        targets.countdown = $(`.col-3.${chest.colorPlatform} .count_time`)
-        targets.startBtn = $(`.col-3.${chest.colorPlatform} .mix_btn`)
-        targets.upgradeBtn = $(`.col-3.${chest.colorPlatform} .upgrade_btn`)
-        targets.readyBtn = $(`.col-3.${chest.colorPlatform} .mix_finish`)
-        targets.openNowBtn = $(`.col-3.${chest.colorPlatform} .now_finish`)
-        targets.platformChest = $(`.col-3.${chest.colorPlatform} .LV${chest.level}`)
+        currentChest = $(`#section_middle_part .col-3.${chest.colorPlatform}`)
+        targets.countdown = currentChest.find('.count_time')
+        targets.startBtn = currentChest.find('.mix_btn')
+        targets.upgradeBtn = currentChest.find('.upgrade_btn')
+        targets.readyBtn = currentChest.find('.mix_finish')
+        targets.openNowBtn = currentChest.find('.now_finish')
+        targets.platformChest = currentChest.find('.LV${chest.level}')
+
+        // targets.platform = $(`#section_middle_part .potion.platform-${chest.colorPlatform}`)
+        //
+        // targets.platform
+        //   .append(`<img src="https://s3-ap-northeast-1.amazonaws.com/ehanlin-web-resource/event-space/img/magicImg/LV${chest.level}.png">`)
+        //
+        // targets.countdown = $(`#section_middle_part .col-3.${chest.colorPlatform} .count_time`)
+        // targets.startBtn = $(`#section_middle_part .col-3.${chest.colorPlatform} .mix_btn`)
+        // targets.upgradeBtn = $(`#section_middle_part .col-3.${chest.colorPlatform} .upgrade_btn`)
+        // targets.readyBtn = $(`#section_middle_part .col-3.${chest.colorPlatform} .mix_finish`)
+        // targets.openNowBtn = $(`#section_middle_part .col-3.${chest.colorPlatform} .now_finish`)
+        // targets.platformChest = $(`#section_middle_part .col-3.${chest.colorPlatform} .LV${chest.level}`)
 
         if (chest.status === 'UNLOCKING') {
           isUnlockingChestExisted = true
