2029add3f78bbb407eb152ef3ac428475af07a08
AsoTiesto <cute17852337569@gmail.com>
2018-12-12 16:34:04 +0800

    調整功能：調整寶藏領取的 btn 為尚未開放領取

--- src/js/magic/event-award-get.js
+++ src/js/magic/event-award-get.js
@@ -28,33 +28,6 @@ define(['jquery', 'ajax', 'w3', 'eventAwardAreZero', 'confirmPopup'],
               </div>
           </div>
         `
-      let sendingPopupHtml = `
-          <div class="confirm-grid-inception-container">
-            <div class="content-block1 confirm-popup-title-font">
-              <span>目前擁有</span>
-            </div>
-            <div class="content-block2">
-              <div>
-                <button class="waiting-btn" type="button">待領取</button>
-              </div>
-              <div>
-                <button class="sending-btn" type="button">出貨中</button>
-              </div>
-              <div>
-                <button class="all-award-btn" type="button">所有寶藏</button>
-              </div>
-            </div>
-            <div class="img-block-left-btn">
-              <img class="left-btn" src="./img/magicImg/previous.svg">
-            </div>
-            <div class="img-block-right-btn">
-              <img class="right-btn" src="./img/magicImg/next.svg">
-            </div>
-            <div class="sending-desc">
-              在點選領取按鈕後，約7-14天工作天為你寄出寶藏喔！
-            </div>
-          </div>
-        `
 
       let getAwardsOnOpenFunc = (awardQuantity, textDesc, textDescSec) => {
         ajax('GET', `/chest/award/`)
@@ -135,7 +108,7 @@ define(['jquery', 'ajax', 'w3', 'eventAwardAreZero', 'confirmPopup'],
         customClass: 'my_treasure_message_box modal-popup-inception-height',
         background: '#a6937c',
         width: '85%',
-        confirmButtonText: '領取寶藏',
+        confirmButtonText: '尚未開放領取',
         showCancelButton: false,
         confirmButtonClass: 'btn message_box_btn_style',
         onOpenFn: () => {
