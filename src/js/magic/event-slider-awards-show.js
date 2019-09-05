2424c3dc3038850398acca4e4e3fb94687e726dc
AsoTiesto <cute17852337569@gmail.com>
2019-09-05 18:23:42 +0800

    調整功能：調整 禮物獎品無法顯示的問題 及 requireJs time out 問題

--- src/js/magic/event-slider-awards-show.js
+++ src/js/magic/event-slider-awards-show.js
@@ -1,81 +1,82 @@
-define(['jquery', 'ajax', 'w3'], ($, ajax, w3) => { // eslint-disable-line
-  return isNovice => {
-    $(window).resize(() => {
-      if (window.matchMedia('(max-width: 800px)').matches) {
-        $('.slide-show-award-introduce .needChestLv').hide()
-        $('.slide-show-award-introduce .awardNotice').hide()
-      } else {
-        $('.slide-show-award-introduce .needChestLv').show()
-        $('.slide-show-award-introduce .awardNotice').show()
-      }
-    })
+define(["jquery", "ajax", "w3"], ($, ajax, w3) => {
+    // eslint-disable-line
+    return isNovice => {
+        $(window).resize(() => {
+            if (window.matchMedia("(max-width: 800px)").matches) {
+                $(".slide-show-award-introduce .needChestLv").hide();
+                $(".slide-show-award-introduce .awardNotice").hide();
+            } else {
+                $(".slide-show-award-introduce .needChestLv").show();
+                $(".slide-show-award-introduce .awardNotice").show();
+            }
+        });
 
-    ajax('GET', `/chest/award/awardConditions`)
-      .then(jsonData => {
-        let awardConditions = jsonData.content
-        let sectionTarget = isNovice ? $('#section_novice') : $('#section_middle_part')
+        ajax("GET", `/chest/award/awardConditions`).then(jsonData => {
+            let awardConditions = jsonData.content;
+            let sectionTarget = isNovice ? $("#section_novice") : $("#section_middle_part");
 
-        for (let index in awardConditions) {
-          let awardCondition = awardConditions[index]
-          let title = awardCondition.title
-          let quantity = parseInt(awardCondition.quantity)
-          let needChestLv = awardCondition.needChestLv
-          let notice = awardCondition.notice
-          let awardId = awardCondition.id
-          let howMany
-          let awardImg
-          let awardTitle
-          let awardInLv
-          let awardFactory
-          let awardNotice
+            for (let index in awardConditions) {
+                let awardCondition = awardConditions[index];
+                let title = awardCondition.title;
+                let quantity = parseInt(awardCondition.quantity);
+                let needChestLv = awardCondition.needChestLv;
+                let notice = awardCondition.notice;
+                let awardId = awardCondition.id;
+                let howMany;
+                let awardImg;
+                let awardTitle;
+                let awardInLv;
+                let awardFactory;
+                let awardNotice;
 
-          if (quantity === 0) {
-            howMany = '沒貨啦'
-          } else if (quantity > 0) {
-            howMany = '還有貨喔'
-          }
+                if (quantity === 0) {
+                    howMany = "沒貨啦";
+                } else if (quantity > 0) {
+                    howMany = "還有貨喔";
+                }
+                console.log(awardId);
+                awardImg = `<img class="gift_photo" src="./img/award/${awardId}.png">`;
+                awardTitle = `<div class="award_title">${title}</div>`;
+                awardInLv = `<div class="award_level">Lv${needChestLv}</div>`;
+                awardFactory = `<div class="award_stock">${howMany}</div>`;
+                awardNotice = `<div class="award_note">${notice}</div>`;
 
-          awardImg = `<img class="gift_photo" src="./img/award/${awardId}.png">`
-          awardTitle = `<div class="award_title">${title}</div>`
-          awardInLv = `<div class="award_level">Lv${needChestLv}</div>`
-          awardFactory = `<div class="award_stock">${howMany}</div>`
-          awardNotice = `<div class="award_note">${notice}</div>`
+                // sectionTarget.find('.gift_photo').append(awardImg)
 
-          sectionTarget.find('.gift_photo').append(awardImg)
+                let rowGiftContentTarget = sectionTarget.find(".row.row_gift_content");
+                rowGiftContentTarget.find(".title-row").append(awardImg);
+                rowGiftContentTarget.find(".title-row").append(awardTitle);
+                rowGiftContentTarget.find(".award_scontnet").append(awardInLv);
+                rowGiftContentTarget.find(".award_scontnet").append(awardFactory);
+                rowGiftContentTarget.find(".notice").append(awardNotice);
+            }
 
-          let rowGiftContentTarget = sectionTarget.find('.row.row_gift_content')
-          rowGiftContentTarget.find('.title-row').append(awardTitle)
-          rowGiftContentTarget.find('.award_scontnet').append(awardInLv)
-          rowGiftContentTarget.find('.award_scontnet').append(awardFactory)
-          rowGiftContentTarget.find('.notice').append(awardNotice)
-        }
+            let sec = 3000;
+            let awardPhotoShow = w3.slideshow(".gift_photo", sec);
+            let awardTitleShow = w3.slideshow(".award_title", sec);
+            let awardLevelShow = w3.slideshow(".award_level", sec);
+            let awardStockShow = w3.slideshow(".award_stock", sec);
+            let awardNoticeShow = w3.slideshow(".award_note", sec);
 
-        let sec = 3000
-        let awardPhotoShow = w3.slideshow('.gift_photo', sec)
-        let awardTitleShow = w3.slideshow('.award_title', sec)
-        let awardLevelShow = w3.slideshow('.award_level', sec)
-        let awardStockShow = w3.slideshow('.award_stock', sec)
-        let awardNoticeShow = w3.slideshow('.award_note', sec)
+            // 上一個
+            sectionTarget.find(".previous_btn img").on("click", () => {
+                awardPhotoShow.next();
+                awardTitleShow.next();
+                awardLevelShow.next();
+                awardStockShow.next();
+                awardNoticeShow.next();
+            });
 
-        // 上一個
-        sectionTarget.find('.previous_btn img').on('click', () => {
-          awardPhotoShow.next()
-          awardTitleShow.next()
-          awardLevelShow.next()
-          awardStockShow.next()
-          awardNoticeShow.next()
-        })
+            // 下一個
+            sectionTarget.find(".next_btn img").on("click", () => {
+                awardPhotoShow.previous();
+                awardTitleShow.previous();
+                awardLevelShow.previous();
+                awardStockShow.previous();
+                awardNoticeShow.previous();
+            });
 
-        // 下一個
-        sectionTarget.find('.next_btn img').on('click', () => {
-          awardPhotoShow.previous()
-          awardTitleShow.previous()
-          awardLevelShow.previous()
-          awardStockShow.previous()
-          awardNoticeShow.previous()
-        })
-
-        //sectionTarget.find('.gift_list .gift_photo').width('115px')
-      })
-  }
-})
+            //sectionTarget.find('.gift_list .gift_photo').width('115px')
+        });
+    };
+});
--- src/js/magic/main.js
+++ src/js/magic/main.js
@@ -1,125 +1,124 @@
 require.config({
-  shim: {
-    dialog: ['jquery'],
-    jqueryCountDown: ['jquery'],
-    w3: {
-      exports: 'w3'
+    waitSeconds: 0,
+    shim: {
+        dialog: ["jquery"],
+        jqueryCountDown: ["jquery"],
+        w3: {
+            exports: "w3"
+        }
+    },
+
+    paths: {
+        /* 銀河星際初始化 */
+        eventClickLink: ["./event-click-link"],
+        eventTotalAssets: ["./event-total-assets"],
+        eventSlideAwardsShow: ["./event-slider-awards-show"],
+        eventSliderAudioBar: ["./event-slider-audio-bar"],
+        eventCountdown: ["./event-countdown"],
+        eventUserStatus: ["./event-user-status"],
+
+        /* 取回藥水與判定狀態 */
+        eventChestGet: ["./chest/event-chest-get"],
+        eventChestDetermine: ["./chest/event-chest-determine"],
+        eventChestBtnOn: ["./chest/event-chest-btn-on"],
+        eventChestStatusDo: ["./chest/event-chest-status-do"],
+
+        /* 藥水 action */
+        eventChestUpgrade: ["./chest/event-chest-upgrade"],
+        eventChestInception: ["./chest/event-chest-inception"],
+        eventChestReady: ["./chest/event-chest-ready"],
+        eventChestOpenImmediately: ["./chest/event-chest-open-immediately"],
+        eventChestOpen: ["./chest/event-chest-open"],
+        eventChestCheck: ["./chest/event-chest-check"],
+
+        eventAwardAreZero: ["./event-award-are-zero"],
+        eventAwardGet: ["./event-award-get"],
+
+        eventCountUp: ["./event-count-up"],
+        eventBonusPopup: ["./event-bonus-popup"],
+        eventOpenFinalPage: ["./event-open-final-page"],
+
+        /* 雲端奇幻魔藥學 */
+        eventTutorialInit: ["./tutorial/event-tutorial-init"],
+
+        /* 雲端排行榜 */
+        eventRankingPopup: ["./ranking/event-ranking-popup"],
+        eventRankingTop: ["./ranking/event-ranking-top"],
+        eventRankingAcademy: ["./ranking/event-ranking-academy"],
+        eventRankingMy: ["./ranking/event-ranking-my"],
+        eventRankingReward: ["./ranking/event-ranking-reward"],
+        eventRankingHistory: ["./ranking/event-ranking-history"],
+
+        /* 初始化 */
+        eventGameBegin: ["./event-game-begin"],
+
+        /* third party */
+        jquery: ["../lib/jquery-3.3.1.min"],
+        w3: ["../lib/w3"],
+        sweetAlert: ["../lib/sweetalert2.all.min"],
+        jqueryCountDown: ["../lib/jquery-time-countdown.min"],
+        countUp: ["../lib/countUp.min"],
+        babelPolyfill: ["../lib/polyfill.min"],
+        cookie: ["../lib/js.cookie.min"],
+        slider: ["../lib/bootstrap-slider.min"],
+
+        /* 共用元件 */
+        confirmPopup: ["../module-utils/confirm-popup"],
+        ajax: ["../module-utils/ajax"],
+        commonUtil: ["../module-utils/common-util"]
+    },
+
+    map: {
+        "*": {
+            jQuery: "jquery"
+        }
     }
-  },
+});
 
-  paths: {
-    /* 銀河星際初始化 */
-    eventClickLink: ['./event-click-link'],
-    eventTotalAssets: ['./event-total-assets'],
-    eventSlideAwardsShow: ['./event-slider-awards-show'],
-    eventSliderAudioBar: ['./event-slider-audio-bar'],
-    eventCountdown: ['./event-countdown'],
-    eventUserStatus: ['./event-user-status'],
-
-    /* 取回藥水與判定狀態 */
-    eventChestGet: ['./chest/event-chest-get'],
-    eventChestDetermine: ['./chest/event-chest-determine'],
-    eventChestBtnOn: ['./chest/event-chest-btn-on'],
-    eventChestStatusDo: ['./chest/event-chest-status-do'],
-
-    /* 藥水 action */
-    eventChestUpgrade: ['./chest/event-chest-upgrade'],
-    eventChestInception: ['./chest/event-chest-inception'],
-    eventChestReady: ['./chest/event-chest-ready'],
-    eventChestOpenImmediately: ['./chest/event-chest-open-immediately'],
-    eventChestOpen: ['./chest/event-chest-open'],
-    eventChestCheck: ['./chest/event-chest-check'],
-
-    eventAwardAreZero: ['./event-award-are-zero'],
-    eventAwardGet: ['./event-award-get'],
-
-    eventCountUp: ['./event-count-up'],
-    eventBonusPopup: ['./event-bonus-popup'],
-    eventOpenFinalPage: ['./event-open-final-page'],
-
-    /* 雲端奇幻魔藥學 */
-    eventTutorialInit: ['./tutorial/event-tutorial-init'],
-
-    /* 雲端排行榜 */
-    eventRankingPopup: ['./ranking/event-ranking-popup'],
-    eventRankingTop: ['./ranking/event-ranking-top'],
-    eventRankingAcademy: ['./ranking/event-ranking-academy'],
-    eventRankingMy: ['./ranking/event-ranking-my'],
-    eventRankingReward: ['./ranking/event-ranking-reward'],
-    eventRankingHistory: ['./ranking/event-ranking-history'],
-
-    /* 初始化 */
-    eventGameBegin: ['./event-game-begin'],
-
-    /* third party */
-    jquery: ['../lib/jquery-3.3.1.min'],
-    w3: ['../lib/w3'],
-    sweetAlert: ['../lib/sweetalert2.all.min'],
-    jqueryCountDown: ['../lib/jquery-time-countdown.min'],
-    countUp: ['../lib/countUp.min'],
-    babelPolyfill: ['../lib/polyfill.min'],
-    cookie: ['../lib/js.cookie.min'],
-    slider: ['../lib/bootstrap-slider.min'],
-
-    /* 共用元件 */
-    confirmPopup: ['../module-utils/confirm-popup'],
-    ajax: ['../module-utils/ajax'],
-    commonUtil: ['../module-utils/common-util']
-  },
-
-  map: {
-    '*': {
-      'jQuery': 'jquery'
-    }
-  }
-})
-
-let requireLoad = require.load
+let requireLoad = require.load;
 require.load = (context, moduleId, url) => {
-  let now = new Date()
-  let year = now.getFullYear()
-  let month = now.getMonth() + 1
-  let date = now.getDate()
-  let hour = now.getHours()
-  let minute = now.getMinutes()
-  let timestamp = `${year}${month}${date}${hour}${minute}`
-  let appVersion = `appVersion=${timestamp}`
-  url = `${url}?${appVersion}`
-  requireLoad(context, moduleId, url)
-}
+    let now = new Date();
+    let year = now.getFullYear();
+    let month = now.getMonth() + 1;
+    let date = now.getDate();
+    let hour = now.getHours();
+    let minute = now.getMinutes();
+    let timestamp = `${year}${month}${date}${hour}${minute}`;
+    let appVersion = `appVersion=${timestamp}`;
+    url = `${url}?${appVersion}`;
+    requireLoad(context, moduleId, url);
+};
 
 require([], () => {
-  /* 一開始沒有return function的 js 必須在這裡require */
-  require(['babelPolyfill'])
-  require(['eventTutorialInit'])
-  require(['eventClickLink'])
-  require(['eventUserStatus'])
-  require(['eventBonusPopup'])
-  require(['eventSliderAudioBar'])
+    /* 一開始沒有return function的 js 必須在這裡require */
+    require(["babelPolyfill"]);
+    require(["eventTutorialInit"]);
+    require(["eventClickLink"]);
+    require(["eventUserStatus"]);
+    require(["eventBonusPopup"]);
+    require(["eventSliderAudioBar"]);
 
-  // 判定使用者裝置
-  let determineDevice = () => {
-    let userAgent = navigator.userAgent
-    let isMobile = /Android|webOS|iPhone|iPad|BlackBerry|IEMobile|Opera Mini/i.test(
-      userAgent
-    )
+    // 判定使用者裝置
+    let determineDevice = () => {
+        let userAgent = navigator.userAgent;
+        let isMobile = /Android|webOS|iPhone|iPad|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
 
-    if (isMobile) {
-      document.getElementById('remind-mask').style.fontSize = '26px'
-    }
-  }
+        if (isMobile) {
+            document.getElementById("remind-mask").style.fontSize = "26px";
+        }
+    };
 
-  // 判定裝置方向
-  let determineOrientation = () => {
-    let remindMaskTarget = document.getElementById('remind-mask')
-    if (window.innerWidth < window.innerHeight) {
-      remindMaskTarget.style.display = ''
-    } else {
-      remindMaskTarget.style.display = 'none'
-    }
-  }
+    // 判定裝置方向
+    let determineOrientation = () => {
+        let remindMaskTarget = document.getElementById("remind-mask");
+        if (window.innerWidth < window.innerHeight) {
+            remindMaskTarget.style.display = "";
+        } else {
+            remindMaskTarget.style.display = "none";
+        }
+    };
 
-  determineDevice()
-  determineOrientation()
-  window.addEventListener('resize', determineOrientation)
-})
+    determineDevice();
+    determineOrientation();
+    window.addEventListener("resize", determineOrientation);
+});
